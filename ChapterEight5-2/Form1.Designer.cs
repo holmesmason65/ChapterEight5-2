2377f9c8e0fe8f5323660f560c2fc1d9ff25ec60
mholmes022726 <mholmes022726@students.iticollege.edu>
2021-07-14 20:21:50 -0500

    Add project files.

---
+++ ChapterEight5-2/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace ChapterEight5_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnCancle = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAddNed = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.txtAuthorID = new System.Windows.Forms.TextBox();
+            this.txtYearBorn = new System.Windows.Forms.TextBox();
+            this.txtAuthorName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(211, 150);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "Next=>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(130, 150);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 1;
+            this.btnPrevious.Text = "<=Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // btnCancle
+            // 
+            this.btnCancle.Location = new System.Drawing.Point(252, 179);
+            this.btnCancle.Name = "btnCancle";
+            this.btnCancle.Size = new System.Drawing.Size(75, 23);
+            this.btnCancle.TabIndex = 2;
+            this.btnCancle.Text = "Cancel";
+            this.btnCancle.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(171, 179);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(90, 179);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnAddNed
+            // 
+            this.btnAddNed.Location = new System.Drawing.Point(90, 208);
+            this.btnAddNed.Name = "btnAddNed";
+            this.btnAddNed.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNed.TabIndex = 5;
+            this.btnAddNed.Text = "Add New";
+            this.btnAddNed.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(171, 208);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(252, 208);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(75, 23);
+            this.btnDone.TabIndex = 7;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            // 
+            // txtAuthorID
+            // 
+            this.txtAuthorID.Location = new System.Drawing.Point(130, 51);
+            this.txtAuthorID.Name = "txtAuthorID";
+            this.txtAuthorID.Size = new System.Drawing.Size(100, 23);
+            this.txtAuthorID.TabIndex = 8;
+            // 
+            // txtYearBorn
+            // 
+            this.txtYearBorn.Location = new System.Drawing.Point(130, 109);
+            this.txtYearBorn.Name = "txtYearBorn";
+            this.txtYearBorn.Size = new System.Drawing.Size(100, 23);
+            this.txtYearBorn.TabIndex = 9;
+            // 
+            // txtAuthorName
+            // 
+            this.txtAuthorName.Location = new System.Drawing.Point(130, 80);
+            this.txtAuthorName.Name = "txtAuthorName";
+            this.txtAuthorName.Size = new System.Drawing.Size(197, 23);
+            this.txtAuthorName.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Author Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Year Born ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Author ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 285);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAuthorName);
+            this.Controls.Add(this.txtYearBorn);
+            this.Controls.Add(this.txtAuthorID);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddNed);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancle);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnCancle;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAddNed;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.TextBox txtAuthorID;
+        private System.Windows.Forms.TextBox txtYearBorn;
+        private System.Windows.Forms.TextBox txtAuthorName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ChapterEight5-2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ChapterEight5_2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection booksConnection;
+        SqlCommand authorsCommand;
+        SqlDataAdapter authorsAdapter;
+        DataTable authorsTable;
+        CurrencyManager authorsManager; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            booksConnection = new SqlConnection(@"Data Source=SQLEXPRESS;
+                                                    AttachDbFileName=C:\Users\mholmes022726\source\repos\ChapterEight5-2\ChapterEight5-2\bin\Debug\netcoreapp3.1\SQLBooksDB.mdf;
+                                                    Integrated Security=True; Connect Timeout=30; User Instance=True");
+            booksConnection.Open(); 
+        }
+    }
+}
